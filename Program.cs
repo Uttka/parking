84ee8b56f89974580c90b688c06e4ecb613a8078
Uttka <andreypivnev@gmail.com>
2023-01-17 15:30:45 +0200

    added funcs and proverka

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using System.IO;
+using System.Text.RegularExpressions;
 
 int menupunkt = 0;
 string vID;
 int bal;
 string vtype;
+int menupunkt2;
 List<Viechle> viechles = new List<Viechle>();
-Console.WriteLine("");
+
 do
 {
     Menu();
@@ -17,71 +19,145 @@ do
 
 void Menu()
 {
-    Console.WriteLine("Введіть елемент меню:\n"+"1)Поставити ТЗ на паркінг\n"+"2)Поповнити баланс ТЗ\n"+"3)Достунпі місця");
+    Console.WriteLine("Введіть елемент меню:\n"+"1)Поставити ТЗ на паркінг\n"+ "2)Забрати\n" + "3)Доступні місця");
     int menupunkt = Convert.ToInt32(Console.ReadLine());
     switch (menupunkt)
     {
         case 1:
-            Console.WriteLine("Поставити транспорт чи забрати?+\n"+"1)Поставити \n"+ "2)Забрати\n");
-            int menupunkt2 = Convert.ToInt32(Console.ReadLine());
-            switch (menupunkt)
-            {   
-                case 1:
-                    addviecle();
-                    break;
-                case 2:
-                    deleteviecle();
-                    break;
-                default:
-                    break;
-            }
+            Console.Clear();
+            Console.WriteLine("Поставити транспорт");
 
-           
-           
+            addviecle();
+
             break;
         case 2:
-            Console.WriteLine("2");
-            Console.WriteLine("на першому місці стоїть авто з айді:" + Convert.ToString(viechles[0]));
-            
+            Console.Clear();
+            Console.WriteLine("Забрати транспорт");
+            deleteviecle();
             break;
         case 3:
-            Console.WriteLine("3");
+            Console.Clear();
+            Console.WriteLine("Доступні місця");
+
+            vieclesinpark();
             break;
         default:
-            Console.WriteLine("Ви ввели неправильний пункт меню");
+            Console.WriteLine("Ви ввели неправильний пункт меню\n");
             break;
     }
 
 }
 void addviecle()
 {
-    Console.WriteLine("Введіть айді транспорту:");
-    vID = Console.ReadLine();
-    Console.WriteLine("Введіть баланс транспорту:");
-    bal = Convert.ToInt32(Console.ReadLine());
-    vtype = Console.ReadLine();
-    viechles.Add(new Viechle() { ViechleId = vID, Viechletype = vtype, Viechlbalance = bal });
-    Console.WriteLine("Транспорт додано");
+    if (viechles.Count<11)
+    {
+        enterelement("айді транспорту");
+        vID = Console.ReadLine();
+        
+        if (idformat(vID))
+        {
+            if (!(viechles.Contains(new Viechle { ViechleId = vID })))
+            {
+
+                enterelement("баланс транспорту");
+
+                bal = Convert.ToInt32(Console.ReadLine());
+              
+                enterelement("тип");
+
+                vtype = Console.ReadLine();
+
+                if (typeformat(vtype))
+                {
+                    viechles.Add(new Viechle() { ViechleId = vID, Viechletype = vtype, Viechlbalance = bal });
+                    Console.Clear();
+                    Console.WriteLine("Транспорт додано");
+                }
+            
+                
+                else
+                {
+                    Console.WriteLine($"Ваш тип {vtype}, але вірний формат:Легкова, Вантажна, Автобус, Мотоцикл") ;
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Транспорт Вже на паркінгу\n");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine($"Ваш айді '{vID}' не підходить під формат: ХХ-YYYY-XX");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("Парк заповнено\n");
+    }
+ 
+}
+void enterelement(string word) 
+{
+    Console.WriteLine($"Введіть {word}:");
+}
+bool idformat(string word)
+{
+    if (word == null)
+    {
+        return false;
+    }
+    var regex = new Regex("^\\b[A-Z]{2}-\\d{4}-\\b[A-Z]{2}$");
+    return regex.IsMatch(word);
+}
+bool typeformat(string type) 
+{
+    if (type == null) 
+    {
+        return false;
+    }
+    else if(type== "Легкова" || type == "Вантажна" || type == "Автобус" || type == "Мотоцикл")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 void deleteviecle() 
 {
     Console.WriteLine("Введіть айді транспорту:");
     vID = Console.ReadLine();
-    viechles.Remove(new Viechle() { ViechleId = vID});
-    Console.WriteLine("Видалено");
+    if (idformat(vID))
+    {
+
+        viechles.Remove(new Viechle() { ViechleId = vID });
+        Console.WriteLine("Видалено\n");
+    }
+    else
+    {
+        Console.WriteLine($"Ваш айді '{vID}' не підходить під формат: ХХ-YYYY-XX");
+    }
 }
+ void vieclesinpark() 
+{
+    if (viechles.Count==10)
+    {
+        Console.WriteLine("Парк зайнято\n");
+    }
+    else if (viechles.Count==0)
+    {
+        Console.WriteLine("Парк не зайнято\n");
+    }
+    else
+    {
+        Console.WriteLine("Парк зайнято з "+Convert.ToString(viechles.Count-10)+" по 10\n");
+    }
+}   
 
-//object Viechle(string ID,double balance)
-//{
-
-//    transport newviechle = new()
-//    {
-//        vehicleID = ID,
-
-//        vehicleBalance = Convert.ToInt32(balance)
-//    };
-//    return newviechle;
-//}
 public class Viechle : IEquatable<Viechle>
 {
     public string ViechleId { get; set; }
@@ -109,5 +185,15 @@ public class Viechle : IEquatable<Viechle>
         if (other == null) return false;
         return (this.ViechleId.Equals(other.ViechleId));
     }
-    // Should also override == and != operators.
+   
 }
+
+//try
+//{
+//    Console.WriteLine("на першому місці стоїть авто з айді:" + Convert.ToString(viechles[0]));
+//}
+//catch (Exception)
+//{
+
+//    Console.WriteLine("Тз не знайдено");
+//}
